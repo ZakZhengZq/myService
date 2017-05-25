27a8437a1d7611a2a15004ba5713441f585af007
byCrazyBStone <zzqworkspace@126.com>
2017-05-25 15:54:39 +0800

    sidebar整体

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,8 @@ var minify_css =require("gulp-minify-css");　//创建 css混淆压缩模块
 var gulp_concat = require('gulp-concat');  //创建 文件合并模块
 
 gulp.task('min-css',function(){  //1
-    gulp.src(['','./static/css/b_base.css'])  //2
-        .pipe(gulp_concat("blog_base.min.css"))//3
+    gulp.src(['','./static/css/'])  //2
+        .pipe(gulp_concat(""))//3
         .pipe(minify_css())//4.混淆即压缩
         .pipe(gulp.dest('./static/css'));//5.输出路径
 });

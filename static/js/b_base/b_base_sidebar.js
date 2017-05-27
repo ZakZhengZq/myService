dee600ac232c8f3278a47de31915a741eddff234
byCrazyBStone <zzqworkspace@126.com>
2017-05-28 00:25:50 +0800

    base_body_inner

--- static/js/b_base/b_base_sidebar.js
+++ static/js/b_base/b_base_sidebar.js
@@ -1,25 +1,3 @@
 /**
  * Created by dell on 2017/5/22.
  */
-$(".sidebar").mouseenter(function() {
-    $(this).addClass("sidebar-hover");
-}).mouseleave(function() {
-    $(this).removeClass("sidebar-hover");
-});
-$(".func-item").mouseenter(function() {
-    $(this).children("div").css({
-        //"left": left,
-        "opacity": "0",
-        "display": "block"
-    }).clearQueue().show().stop().animate({
-        //"left": left - 15,
-        "opacity": "1"
-    }, "fast");
-}).mouseleave(function() {
-    $(this).children("div").stop().delay().animate({
-        //"left": left,
-        "opacity": "0"
-    }, "fast", function() {
-        $(this).hide()
-    });
-});

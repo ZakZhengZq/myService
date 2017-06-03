83ac266a238671567661aca78b4af2588fa9c26e
Hunter <zzqworkspace@126.com>
2017-06-04 02:02:03 +0800

    完成留言页面

--- static/js/b_base/b_base_chat.js
+++ static/js/b_base/b_base_chat.js
@@ -28,6 +28,7 @@ function addMessage(list, msg) {
         scrollTop: $('#message-list').height()
     }, 1000);
 }
+var id,img,name='';
 $(function () {
     var vmMessageList = new Vue({
         el: '#message-list',
@@ -46,6 +47,11 @@ $(function () {
         var data = event.data;
         console.log(data);
         var msg = JSON.parse(data);
+        if (id==''||img==''|| name==''){
+            id=msg["user"].id;
+            img=msg["user"].image;
+            name=msg["user"].name;
+        }
         if (msg.type === 'list') {
             vmUserList.users = msg.data;
         } else if (msg.type === 'join') {
@@ -89,7 +95,7 @@ $(function () {
                             <span class="time" v-text="itemtext.date">今日16:23</span>\
                         </div>\
                         <div class="inner">\
-                            <p class="text" v-text="itemtext.text">所有的留言目前都是匿名哦</p>\
+                            <p v-bind:id="itemtext.id" class="text" v-html="itemtext.text"></p>\
                         </div>\
                     </div>\
                 </div>\
@@ -98,20 +104,41 @@ $(function () {
     var vmLeaveList = new Vue({
         el: '#leave',
         data: {
-            leaveText: '',
             leaveTexts: [
-                {img:'/static/images/photo2.png',name:'疯狂大石头',date:'2017年5月',text:'所有的评论目前都是匿名的哦'},
-                {img:'/static/images/photo2.png',name:'疯狂大石头',date:'2017年5月',text:'念奴娇•赤壁怀古【宋】苏轼大江东去，浪淘尽，千古风流人物。故垒西边，人道是，三国周郎赤壁。乱石穿空，惊涛拍岸，卷起千堆雪。江山如画，一时多少豪杰。遥想公瑾当年，小乔初嫁了，雄姿英发。羽扇纶巾，谈笑间，樯橹灰飞烟灭。故国神游，多情应笑我，早生华发。人生如梦，一樽还酹江月。'},
-                {img:'/static/images/photo2.png',name:'疯狂大石头',date:'2017年5月',text:'江城子•密州出猎【宋】苏轼老夫聊发少年狂，左牵黄，右擎苍，锦帽貂裘，千骑卷平冈。为报倾城随太守，亲射虎，看孙郎。酒酣胸胆尚开张，鬓微霜，又何妨？持节云中，何日遣冯唐？会挽雕弓如满月，西北望，射天狼。'},
-                {img:'/static/images/photo2.png',name:'疯狂大石头',date:'2017年5月',text:'永遇乐•京口北固亭怀古【宋】辛弃疾千古江山，英雄无觅，孙仲谋处。舞榭歌台，风流总被，雨打风吹去。斜阳草树，寻常巷陌，人道寄奴曾住。想当年，金戈铁马，气吞万里如虎。元嘉草草，封狼居胥，赢得仓皇北顾。四十三年，望中犹记，烽火扬州路。可堪回首，佛狸祠下，一片神鸦社鼓。凭谁问，廉颇老矣，尚能饭否？'},
+                {id:'test1',img:'/static/images/photo2.png',name:'疯狂大石头',date:'2017年5月',text:'所有的评论目前都是匿名的哦'},
+                {id:'test2',img:'/static/images/photo2.png',name:'疯狂大石头',date:'2017年5月',text:'念奴娇•赤壁怀古【宋】苏轼大江东去，浪淘尽，千古风流人物。故垒西边，人道是，三国周郎赤壁。乱石穿空，惊涛拍岸，卷起千堆雪。江山如画，一时多少豪杰。遥想公瑾当年，小乔初嫁了，雄姿英发。羽扇纶巾，谈笑间，樯橹灰飞烟灭。故国神游，多情应笑我，早生华发。人生如梦，一樽还酹江月。'},
+                {id:'test3',img:'/static/images/photo2.png',name:'疯狂大石头',date:'2017年5月',text:'江城子•密州出猎【宋】苏轼老夫聊发少年狂，左牵黄，右擎苍，锦帽貂裘，千骑卷平冈。为报倾城随太守，亲射虎，看孙郎。酒酣胸胆尚开张，鬓微霜，又何妨？持节云中，何日遣冯唐？会挽雕弓如满月，西北望，射天狼。'},
+                {id:'test4',img:'/static/images/photo2.png',name:'疯狂大石头',date:'2017年5月',text:'永遇乐•京口北固亭怀古【宋】辛弃疾千古江山，英雄无觅，孙仲谋处。舞榭歌台，风流总被，雨打风吹去。斜阳草树，寻常巷陌，人道寄奴曾住。想当年，金戈铁马，气吞万里如虎。元嘉草草，封狼居胥，赢得仓皇北顾。四十三年，望中犹记，烽火扬州路。可堪回首，佛狸祠下，一片神鸦社鼓。凭谁问，廉颇老矣，尚能饭否？'},
             ]
         },
         methods: {
             addNewText: function () {
-                this.leaveTexts.push(this.leaveText)
-                this.leaveText = ''
+                var newText={};
+                newText.img='/static/images/'+img+'.png';
+                newText.name=name;
+                newText.date=this.getDate();
+                newText.text=document.getElementById('editor').innerHTML;
+                this.leaveTexts.push(newText);
+                document.getElementById('editor').innerHTML='';
+            },
+            getDate:function getNowFormatDate() {
+                var date = new Date();
+                var seperator1 = "-";
+                var seperator2 = ":";
+                var month = date.getMonth() + 1;
+                var strDate = date.getDate();
+                if (month >= 1 && month <= 9) {
+                    month = "0" + month;
+                }
+                if (strDate >= 0 && strDate <= 9) {
+                    strDate = "0" + strDate;
+                }
+                var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+                    + " " + date.getHours() + seperator2 + date.getMinutes()
+                    + seperator2 + date.getSeconds();
+                return currentdate;
             }
-        }
+        },
     });
 
 

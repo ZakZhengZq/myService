386271d19e41b9659b7066112403999546e6dccc
Hunter <zzqworkspace@126.com>
2017-06-08 23:37:04 +0800

    文章图片

--- model/config.js
+++ model/config.js
@@ -2,7 +2,11 @@
  * Created by dell on 2017/4/30.
  */
 var config = {
-   
+    database: 'personalBlog', // 使用哪个数据库
+    username: 'admin', // 用户名
+    password: 'admin123', // 口令
+    host: '119.28.86.155', // 主机名
+    port: 3306 // 端口号，MySQL默认3306
 };
 
 module.exports = config;
